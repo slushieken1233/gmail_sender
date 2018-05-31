68bd7b61be7ab42f5084b9da890d7b5bd0aa512c
Sebastian <ssebs@ymail.com>
2018-05-30 23:43:57 -0700

    Added a simple cli

--- gmail_sender.py
+++ gmail_sender.py
@@ -1,11 +1,23 @@
 # first we import libraries
 import smtplib
+import getpass
+
+# python 2/3 compatibility: https://stackoverflow.com/a/21731110
+try:
+	input = raw_input
+except NameError:
+	pass
+
+# get gmail info from console
+username = input("Enter your gmail account: ")
+password = getpass.getpass("Enter your gmail password: ")
+to = input("Enter the reciepient : ")
+message = input("Enter your message: ")
 
 # set our variables/account info
-sender_email_id = "<Gmail username>"
-sender_password = "<Gmail Password>"
-message = "<Email Message>"
-recipient_email_id = "<Recipient Email Address>"
+sender_email_id = username
+sender_password = password
+recipient_email_id = to
 
 # next we create the session with definitions
 session = smtplib.SMTP('smtp.gmail.com', 587)
